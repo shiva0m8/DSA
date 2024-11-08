6ab84c5659a515dc815497ab1ada6d3da58fd039
Shivam Poojara <poojarashivam08@outlook.com>
2024-11-08 18:23:47 +0530

    array hard question 3 and 4

--- ArrayProblems/Hard/index.js
+++ ArrayProblems/Hard/index.js
@@ -44,7 +44,74 @@ function majorityElements3rdHalf(arr) {
 	return outputArr;
 }
 
-console.log(majorityElements3rdHalf([2, 2]));
+//console.log(majorityElements3rdHalf([2, 2]));
 
 //optimal solution : Extended Boyer Moore’s Voting Algorithm
 //remaining
+
+//3. 3 SUM
+
+function calculate3Sum(arr) {
+	arr.sort((a, b) => a - b);
+	target = 0;
+	let ans = [];
+	let arrLen = arr.length;
+	for (let i = 0; i < arrLen; i++) {
+		if (i === 0 || (i !== 0 && arr[i - 1] !== arr[i])) {
+			let j = i + 1;
+			let k = arrLen - 1;
+			while (j < k) {
+				let sum = arr[i] + arr[j] + arr[k];
+				if (sum < target) {
+					j++;
+				} else if (sum > target) {
+					k--;
+				} else {
+					ans.push([arr[i], arr[j], arr[k]]);
+					j++;
+					k--;
+					while (arr[j] === arr[j - 1] && j < k) j++;
+					while (arr[k] === arr[k + 1] && j < k) k--;
+				}
+			}
+		}
+	}
+	return ans;
+}
+
+//console.log(calculate3Sum([-1, 0, 1, 2, -1, -4]));
+
+//4. 4 Sum
+
+function calculate4Sum(arr, target) {
+	arr.sort((a, b) => a - b);
+	let ans = [];
+	let arrLen = arr.length;
+	for (let i = 0; i < arrLen; i++) {
+		if (i === 0 || (i !== 0 && arr[i - 1] !== arr[i])) {
+			for (j = i + 1; j < arrLen; j++) {
+				if (j === i + 1 || (j !== i + 1 && arr[j - 1] !== arr[j])) {
+					let k = j + 1;
+					let l = arrLen - 1;
+					while (k < l) {
+						let sum = arr[i] + arr[j] + arr[k] + arr[l];
+						if (sum < target) {
+							k++;
+						} else if (sum > target) {
+							l--;
+						} else {
+							ans.push([arr[i], arr[j], arr[k], arr[l]]);
+							k++;
+							l--;
+							while (arr[k] === arr[k - 1] && k < l) k++;
+							while (arr[l] === arr[l + 1] && k < l) l--;
+						}
+					}
+				}
+			}
+		}
+	}
+	return ans;
+}
+
+console.log(calculate4Sum([1, 0, -1, 0, -2, 2], 0));
